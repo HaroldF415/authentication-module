42a26f549e80c03dad970574cae7e28865e8b6d9
Harold Fachin <harold.f415@gmail.com>
2020-07-20 15:38:33 -0400

    final level of authentication

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+
 
 const app = express();
 
@@ -33,23 +37,88 @@ mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
+  secret: String
 });
 
-userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose); //, {usernameUnique: false}
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done){
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+  User.findById(id, function(err, user){
+    done(err, user);
+  });
+});
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+passport.use(new GoogleStrategy({
+
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/authentication-module",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+
+  },
+
+  function(accessToken, refreshToken, profile, done) {
+      console.log("GOOGLE PROFILE:" + profile);
+      console.log("--------------------------------------------------------------------------");
+       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+         return done(err, user);
+       });
+
+  }
+
+));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FB_APPID,
+    clientSecret: process.env.FB_APPSECRET,
+    callbackURL: "http://localhost:3000/auth/google/authentication-module"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log(profile);
+    console.log("--------------------------------------------------------------------------");
+    User.findOrCreate({ facebookId: profile.id }, function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
 
 app.get("/", function(req, res) {
 
     res.render("home");
 
 });
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] }));
+
+app.get("/auth/google/authentication-module",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    res.redirect("/secrets");
+  });
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook', { scope: ["profile"] }));
+
+app.get('/auth/facebook/authentication-module',
+  passport.authenticate('facebook', { successRedirect: '/secrets',
+                                      failureRedirect: '/login' }));
 
 app.get("/register", function(req, res) {
 
@@ -65,10 +134,19 @@ app.get("/login", function(req, res) {
 
 app.get("/secrets", function(req, res) {
 
-    if(req.isAuthenticated())
-      res.render("secrets");
-    else
-      res.redirect("/login");
+    // if(req.isAuthenticated())
+    //   res.render("secrets");
+    // else
+    //   res.redirect("/login");
+
+    User.find({secret: {$ne: null} }, function(err, foundUsers){
+      if(err)
+        console.log(err);
+      else{
+        if(foundUsers)
+          res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    });
 
 });
 
@@ -76,6 +154,15 @@ app.get("/logout", function(req, res){
 
   req.logout();
   res.redirect("/");
+
+});
+
+app.get("/submit", function(req, res){
+
+  if(req.isAuthenticated())
+    res.render("submit");
+  else
+    res.redirect("/login");
 
 });
 
@@ -118,7 +205,25 @@ app.post("/login", function(req, res){
 
 }); // ends app.post()
 
+app.post("/submit", function(req, res){
 
+  const submittedSecret = req.body.secret;
+
+  User.findById(req.user.id, function(err, foundUser){
+    if(err)
+      console.log(err);
+    else{
+      if(foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+
+  });
+
+});
 
 let port = process.env.PORT;
 
